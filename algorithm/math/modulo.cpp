783441abb6a8b566da0e1d2d9c54c61a403c2c14
Ho-mmd <ws000617@gmail.com>
2025-05-09 15:43:42 +0900

    Modular Inverse

--- algorithm/math/modulo.cpp
+++ algorithm/math/modulo.cpp
@@ -1,6 +1,33 @@
 #include <iostream>
 
+#define MOD 1e9+7
+
 using namespace std;
+
+/* Modular Inverse
+ * m must be a prime number
+ * a % m != 0
+ * 
+ * a^(m - 1) ≡ 1 (mod m)
+ * a^(m - 2) ≡ a^(-1) (mod m)
+ * Inverse a = a^(m - 2) (mod m)
+ */
+
+long long modpow(long long a, long long b, int mod = MOD) {
+		long long res = 1;
+		
+		a %= mod;
+
+		while (b > 0) {
+				if (b % 2 == 1) 
+						res = (res * a) % mod;
+
+				a = (a * a) % mod;
+				b /= 2;
+		}
+
+		return res;
+}
 
 int main() {
 		int A, B, m;
@@ -13,9 +40,10 @@ int main() {
 		if (re < 0)
 				re += m;
 
-		cout << (A + B) % m << " " << ((A % m) + (B % m)) % m;
-		cout << (A - B) % m << " " << ((A % m) - (B % m)) % m;
-		cout << (A * B) % m << " " << ((A % m) * (B % m)) % m;
+		cout << (A + B) % m << " " << ((A % m) + (B % m)) % m << "\n";
+		cout << (A - B) % m << " " << ((A % m) - (B % m)) % m << "\n";
+		cout << (A * B) % m << " " << ((A % m) * (B % m)) % m << "\n";
+		cout << "Mod Inverse " << modpow(A, MOD - 2, MOD) << "\n";
 
 		return 0;
 }
